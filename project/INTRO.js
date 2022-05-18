353eb86cf287896390b41f2e24da79767e679a4f
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-05-18 20:17:59 +0530

    allfile

---
+++ project/INTRO.js
@@ -0,0 +1,32 @@
+// let hobbies = ["hi", "bye", "hello"];
+// alert(hobbies[0]);
+
+// function name() {
+//   alert("hi buddy");
+// }
+// name();
+
+// let age = 18;
+
+// let tr = function name(age) {
+//   return age;
+// };
+
+// alert(tr);
+
+// console.log(16 + 7);
+
+// console.log("max" + "Deepesh");
+// console.log(4 ** 3);
+// console.log(2 + "2");
+
+// let name = "deepesh";
+// console.log(name.length);
+// DOM document object model
+// // console.log(window);
+// console.log(window.document);
+// console.dir(document);
+// document.body.children[0]
+// let another = document.querySelector("h1");
+// another.remove();
+// very important query selector
