e43e24bec8f91b4f205a333c4419280d18153a38
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-05-19 12:12:08 +0530

    learn javascript

--- project/INTRO.js
+++ project/INTRO.js
@@ -1,3 +1,4 @@
+// important to learn java script.
 // let hobbies = ["hi", "bye", "hello"];
 // alert(hobbies[0]);
 
@@ -33,3 +34,120 @@
 
 // add event listner
 // ==,===,>=,<=
+//document.queryselector(".class").click() (automatically get clicked)
+// document.getElementById("id name").click()
+// ************************************************************************************************************************************
+// f1
+// <div id=​"f1" class=​"container">​…​</div>​
+// f1.innerHTML="hello deepesh"
+// 'hello deepesh'
+//         f1.innerHTML= "<b>deepesh</b>"
+// '<b>deepesh</b>'
+// ************************************************************************************************************************************
+// select element
+// document.getElementById("h1")
+
+// null
+// document.getElementsByTagName("h1")
+// HTMLCollection(2) [h1, h1]
+// document.getElementsByTagName("h1")[0]
+// <h1>​Null point​</h1>​
+// document.getElementsByTagName("h1")[0].style.fontSize="88px"
+// '88px'
+// ************************************************************************************************************************************
+// document.getElementsByTagName("h1")[0].style.visibility = "hidden";
+// // document.getElementsByTagName("h1")[0].style.display = "none";
+// console.log("we will win");
+// document.write("hey guys this is written by document write");
+// console.warning("warn"); warn you
+// console.error("this");
+// console.clear()
+// console.assert(4 == 5);
+// ************************************************************************************************************************************
+
+// javascript variable
+
+// // contain store value
+// var number = 34;
+// console.log(number);
+
+// var marks = {
+//   rt: 34,              //objects
+//   ry: 32,
+// };
+
+// console.log(marks.rt);
+// var a = true;
+// var b = false;
+
+// console.log(a, b);
+
+// ************************************************************************************************************************************
+// var und = undefined;
+// var und; //then also undefined very important
+// console.log(und);
+// var und = null; //null is different
+// console.log(und);
+
+// arrays in java script
+
+//* primitive data type: undefined,null,nummber,string,boolean,symbol*
+// *Refernce data type: Arrays and objects*
+// ************************************************************************************************************************************
+// var arr = [1, 2, 3, 4, 4];
+
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// var arr = [1, 2, 3, 4, "bablu"];
+
+// console.log(arr[4]);
+// ************************************************************************************************************************************
+// Operators in java script
+// // console.log(3 * 90);
+// console.log(4 < 3);
+// console.log(4 > 3);
+//&& || !
+// ************************************************************************************************************************************
+// functions;
+// function avg(a, b) {
+//   return (a + b) / 2;
+// }
+
+// c1 = avg(4, 6);
+
+// console.log(c1);
+// if (5 > 4) {
+//   console.log("BYE");
+// }
+// if (4 > 5) {
+//   console.log("BYE");
+// }
+// https://youtu.be/hKB-YGF14SY?t=5899
+// ************************************************************************************************************************************
+// for loop
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// arr.forEach(function (element) {
+//   console.log(element); //very important syntax
+// });
+
+// let j = 0; //let should be used
+// const a=0; //can't be changed
+// while (j < arr.length) {
+//   console.log(arr[j]);
+//   j++;
+// }
+
+// do {
+  
+// } while (condition);
+
+// break and continue statment
+
+let
