b57ceb6163e4271e9787d4fb88f73c5fc116c034
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-06-30 01:19:47 +0530

    learningtakestime

---
+++ my-app/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const SaveExpenseDatahandler = (enteredexpensedata) => {
+    const expenseData = {
+      ...enteredexpensedata,
+      id: Math.random().toString(),
+    };
+
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm SaveExpenseData={SaveExpenseDatahandler} />
+      {/* ACT AS A PROP */}
+    </div>
+  );
+};
+
+export default NewExpense;
